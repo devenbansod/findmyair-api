8f12afa20d91d2959fd4546738079b9d61a0dbac
Deven Bansod <devenbansod.bits@gmail.com>
2019-10-20 22:46:48 -0400

    switch to using gunicorn instead

--- app.py
+++ app.py
@@ -7,22 +7,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+
+    if not request.args.get('lat1'):
+        return 'lat1 not provided'
+
+    if not request.args.get('lat2'):
+        return 'lat2 not provided'
+
+    if not request.args.get('long1'):
+        return 'long1 not provided'
+
+    if not request.args.get('long2'):
+        return 'long2 not provided'
+
     lat1 = float(request.args.get('lat1'))
     lat2 = float(request.args.get('lat2'))
     long1 = float(request.args.get('long1'))
     long2 = float(request.args.get('long2'))
-
-    if not lat1:
-        return 'lat1 not provided'
-
-    if not lat2:
-        return 'lat2 not provided'
-
-    if not long1:
-        return 'long1 not provided'
-
-    if not long2:
-        return 'long2 not provided'
 
     coords_1 = (lat1, long1)
     coords_2 = (lat2, long2)
