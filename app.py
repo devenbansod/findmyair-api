abc002832a397d0eace2af023607e2c14a272627
KathanKP <kathan.kashiparekh@gatech.edu>
2019-11-25 19:20:05 -0500

    Adding all metrics to rank airbnbs

--- app.py
+++ app.py
@@ -6,17 +6,23 @@ from flask import jsonify
 import os
 import csv
 from models import listings
+import numpy as np
 
 from models import taxicost
 
 app = Flask(__name__)
 API_KEY = os.environ.get('API_KEY', None)
 POIS = None
+CRIME_SCORES = None
+TAXI_COST = None
 
 with open('./data/pois.json', 'r') as pois_file:
     POIS = {
         "listings": json.loads(pois_file.read())
     }
+
+CRIME_SCORES = np.load("data/crime_scores.npy")
+TAXI_COST = np.load("data/travel_cost_matrix.npy")
 
 # MAIN APIS
 @app.route('/pois', methods = ['GET'])
@@ -36,7 +42,8 @@ def get_suggested_airbnbs():
 
     # [ {id, url, cost_score, travel_score, safety_score, lat, lng, suitability_score} ]
     listings_with_scores = get_listings_with_scores(cost_pref, travel_cost_pref, safety_pref, iternaries)
-    listings_with_scores.sort(key=lambda x: x['suitability_score'], reverse=True)
+    listings_with_scores = filter_suggested_airbnbs(listings_with_scores)
+    listings_with_scores.sort(key=lambda x: x['suitability_score']  )
 
     page = int(request.args.get('page')) if request.args.get('page') != None else 1
     count = int(request.args.get('count')) if request.args.get('count') != None else 10
@@ -56,21 +63,32 @@ def get_travel_cost():
     return str(taxicost.avg_cost(data['pickup_lat'], data['pickup_lng'], data['drop_lat'], data['drop_lng']))
 
 # print(taxicost.avg_cost("40.69509499999986","-74.18448899999997","40.61480534006002","-73.83009815098022"))
+def filter_suggested_airbnbs(suggestions):
+    suggestions = [ab for ab in suggestions if (ab['travel_score']>0.0)]
+    return suggestions
 
 def get_listings_with_scores(cost_pref, travel_cost_pref, safety_pref, iternaries):
     LISTINGS = listings.get_all_listings()
     ret = []
 
-    for airbnb in LISTINGS.itertuples():
-        cost_score = airbnb[listings.get_column_index('cost_score')]
-        suitability_score = cost_score * cost_pref
+    for index, airbnb in enumerate(LISTINGS.itertuples()):
+        cost_score = airbnb[listings.get_column_index('cost_score')] 
+        crime_score = CRIME_SCORES[index,0]
+        taxi_cost = 0.0
+        for day in iternaries:
+            taxi_cost += ((TAXI_COST[index, day[0]] + TAXI_COST[index, day[-1]])/2)
+        taxi_cost /= len(iternaries)
+
+        suitability_score = ((cost_score * cost_pref) + (crime_score * safety_pref) + (taxi_cost* travel_cost_pref)) * 0.01
 
         ret.append({
             'id': airbnb[listings.get_column_index('id')],
             'url': airbnb[listings.get_column_index('listing_url')],
             'latitude': airbnb[listings.get_column_index('latitude')],
             'longitude': airbnb[listings.get_column_index('longitude')],
-            'cost_score': airbnb[listings.get_column_index('cost_score')],
+            'cost_score': cost_score,
+            'safety_score': crime_score,
+            'travel_score': taxi_cost,
             'suitability_score': suitability_score
         })
 
