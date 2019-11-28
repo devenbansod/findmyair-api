1234cf8eb0244771bd8255a89f00d9004fa59e06
KathanKP <kathan.kashiparekh@gatech.edu>
2019-11-28 11:30:05 -0500

    Resolving build fail

--- models/taxicost.py
+++ models/taxicost.py
@@ -55,38 +55,38 @@ def get_neighbourhood_id(lat, lng):
 
     return neighbourhood_id;
 
-def avg_cost(pickup_lat, pickup_lng, drop_lat, drop_lng, brooklyn=False, brooklyn_pickup=False):
-    pickup_lat = float(pickup_lat)
-    pickup_lng = float(pickup_lng)
-    drop_lat = float(drop_lat)
-    drop_lng = float(drop_lng)
+# def avg_cost(pickup_lat, pickup_lng, drop_lat, drop_lng, brooklyn=False, brooklyn_pickup=False):
+#     pickup_lat = float(pickup_lat)
+#     pickup_lng = float(pickup_lng)
+#     drop_lat = float(drop_lat)
+#     drop_lng = float(drop_lng)
 
-    pickup_id = get_neighbourhood_id(pickup_lat, pickup_lng)
-    drop_id = get_neighbourhood_id(drop_lat, drop_lng)
-    if pickup_id==103:
-        pickup_id==12
-    if drop_id==103:
-        drop_id=12
-    if brooklyn:
-        if brooklyn_pickup:
-            pickup_id=33
-        else:
-            drop_id=33
+#     pickup_id = get_neighbourhood_id(pickup_lat, pickup_lng)
+#     drop_id = get_neighbourhood_id(drop_lat, drop_lng)
+#     if pickup_id==103:
+#         pickup_id==12
+#     if drop_id==103:
+#         drop_id=12
+#     if brooklyn:
+#         if brooklyn_pickup:
+#             pickup_id=33
+#         else:
+#             drop_id=33
 
-    avg_cost = 0
-    count = 0
-    total_cost = 0
+#     avg_cost = 0
+#     count = 0
+#     total_cost = 0
 
-    if pickup_id != -1 and drop_id != -1:
-        cost_going = TRIP_COSTS[(TRIP_COSTS['PULocationID']==pickup_id) & (TRIP_COSTS['DOLocationID']==drop_id)]
-        cost_returning = TRIP_COSTS[(TRIP_COSTS['PULocationID']==drop_id) & (TRIP_COSTS['DOLocationID']==pickup_id)]
-        combined_cost = cost_going.append(cost_returning, ignore_index=True)
-        if len(combined_cost) == 0:
-            # One poi doesnt exist in map? Id=-1? Brooklyn bridge falls in no neighbourhood.
-            return 0
-        avg_cost = np.mean(combined_cost['total_amount'])
+#     if pickup_id != -1 and drop_id != -1:
+#         cost_going = TRIP_COSTS[(TRIP_COSTS['PULocationID']==pickup_id) & (TRIP_COSTS['DOLocationID']==drop_id)]
+#         cost_returning = TRIP_COSTS[(TRIP_COSTS['PULocationID']==drop_id) & (TRIP_COSTS['DOLocationID']==pickup_id)]
+#         combined_cost = cost_going.append(cost_returning, ignore_index=True)
+#         if len(combined_cost) == 0:
+#             # One poi doesnt exist in map? Id=-1? Brooklyn bridge falls in no neighbourhood.
+#             return 0
+#         avg_cost = np.mean(combined_cost['total_amount'])
 
-    return avg_cost
+#     return avg_cost
 
 def check_if_point_falls_in_rect(lat1, lng1, lat2, lng2, lat, lng):
     if (lat >= min(lat1, lat2) and lat <= max(lat1, lat2)) and (lng >= min(lng1, lng2) and lng <= max(lng1, lng2)):
